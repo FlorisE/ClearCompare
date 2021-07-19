c0b937790073f12c6dafbfa2a9dfdaf9867bb0b4
Floris Erich <floris.erich@aist.go.jp>
2021-07-19 16:32:18 +0900

    Removed port parameter as it didn't really seem necessary for internal communication

--- implicit_depth/run.sh
+++ implicit_depth/run.sh
@@ -1,2 +1,2 @@
 xhost +local:root
-sudo docker run --gpus 1 -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e NVIDIA_DRIVER_CAPABILITIES=all -e NVIDIA_VISIBLE_DEVICES=all --privileged --device=/dev:/dev -h $HOSTNAME -p 50010:50010 implicit_depth_cuda114
+sudo docker run --gpus 1 -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e NVIDIA_DRIVER_CAPABILITIES=all -e NVIDIA_VISIBLE_DEVICES=all --privileged --device=/dev:/dev -h $HOSTNAME implicit_depth_cuda114
